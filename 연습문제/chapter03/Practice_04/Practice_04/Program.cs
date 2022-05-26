7044180a88955fb6bea02c4d71f59317b01412bd
yechan <dnjsdp258@gmail.com>
2022-05-26 22:00:34 +0900

    2022-05-26 study
    "이것이 c# 이다"  ~173p (~chapter3 연습문제까지)[알라딘 ebook기준 페이지]

---
+++ 연습문제/chapter03/Practice_04/Practice_04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Practice_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 240;
+            Console.WriteLine(Convert.ToString(a,2));
+        }
+    }
+}
