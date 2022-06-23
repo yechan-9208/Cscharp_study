3c02b9b5a5f4f58c06321348e36ef5281136246a
yechan9208 <dnjsdp258@gmail.com>
2022-06-23 23:02:06 +0900

    2022-06-23 study
    "이것이 c# 이다"  ~274p (~chapter7.14까지)[알라딘 ebook기준 페이지]

--- 연습문제/chapter03/problem1/problem1/problem3.cs
+++ 연습문제/chapter03/problem1/problem1/problem3.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace problems
-{
-    internal class problem3
-    {
-        static void Main(string[] args)
-        {
-            int a = 3;
-            int b = 4;
-            int resulta = 0;
+// namespace problems
+// {
+//     internal class problem3
+//     {
+//         static void Main(string[] args)
+//         {
+//             int a = 3;
+//             int b = 4;
+//             int resulta = 0;
 
-            plus(a, b, out resulta);
+//             plus(a, b, out resulta);
 
-            Console.WriteLine("{0} + {1} = {2}",a,b,resulta);
+//             Console.WriteLine("{0} + {1} = {2}",a,b,resulta);
 
-            double x = 2.4;
-            double y = 3.1;
-            double resultb = 0;
+//             double x = 2.4;
+//             double y = 3.1;
+//             double resultb = 0;
 
-            plus(x, y, out resultb);
+//             plus(x, y, out resultb);
 
-            Console.WriteLine("{0} + {1} = {2}", a, b, resultb);
-        }
+//             Console.WriteLine("{0} + {1} = {2}", a, b, resultb);
+//         }
 
-        public static void plus(int a, int b, out int c)
-        {
-            c = a + b;
-        }
+//         public static void plus(int a, int b, out int c)
+//         {
+//             c = a + b;
+//         }
 
-        public static void plus(double a, double b, out double c)
-        {
-            c = a + b;
-        }
-    }
-}
+//         public static void plus(double a, double b, out double c)
+//         {
+//             c = a + b;
+//         }
+//     }
+// }
